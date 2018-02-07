28a524146192a87b2e1aa76ce124fc49da3943c2
Larry Hudson <Lhudson322@gmail.com>
2018-02-06 19:07:54 -0500

    added video

---
+++ greeting.js
@@ -0,0 +1,14 @@
+//alert that ask for your name
+var name = prompt('What is your name?');
+
+var checkName = function(){
+  if(name === "") {
+    name = prompt('What is your name, for real this time?');
+
+    checkName(); // repeats until a name exists
+  }
+}
+
+checkName(); // kicks off the name-checking the first time
+
+alert('Hello ' + name);
