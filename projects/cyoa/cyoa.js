fe5043751607361e424764018fe1c6a72ed7d029
Larry Hudson <Lhudson322@gmail.com>
2018-02-08 22:22:51 -0500

    made buttons

--- projects/cyoa/cyoa.js
+++ projects/cyoa/cyoa.js
@@ -6,7 +6,7 @@ alert('Hello ' + name);
 var response = prompt("You walk into techtown class and one guy tells the other guy he looks like his father. Type 'yes' if they need a DNA test, type 'no' if the DNA will not help");
 
 if(response === "yes"){
-    response = prompt("You may be a dead beat dad!!!! You are Dante");
+    response = prompt("You may be a dead beat dad!!!! You are Dante's father");
 } else if (response === "no") {
     response = prompt("You just look like someone else");
 } else {
