e9a1f35f82a10a574d5cc9dfd5d8da97f286b07c
Larry Hudson <Lhudson322@gmail.com>
2018-02-15 20:54:40 -0500

    added buttons for group

--- projects/rps/rps.js
+++ projects/rps/rps.js
@@ -1,46 +1,47 @@
-var userChoice = prompt("Choose Rock, Paper, or Scissors by typing 'R', 'P', or 'S'");
-
-var rng = Math.random();
-var computerChoice = "R";
-
-if(rng > 0.66) {
-    computerChoice = "P";
-} else if (rng > 0.33) {
-    computerChoice = "S";
-
-}
-
-
-var userWins = "You WIN!";
-var computerWins = "The computer wins!";
-
-if(computerChoice !== userChoice){
-    if(computerChoice === "R"){
-        if (userChoice === "S") {
-            alert(computerWins);
-        } else {
-            alert(userWins);
-        }
-    } else if (computerChoice === "P") {
-        if( userChoice === "R") {
-            alert(computerWins);
-        } else {
-            alert(userWins);
-        }
-    } else {
-        if( userChoice === "P") {
-            alert(computerWins);
-        } else {
-            alert(userWins);
-        }
-    }
-} else {
-    alert('Tie!');
-}
+// var userChoice = prompt("Choose Rock, Paper, or Scissors by typing 'R', 'P', or 'S'");
+//
+// var rng = Math.random();
+// var computerChoice = "R";
+//
+// if(rng < 0.66) {
+//     computerChoice = "P";
+// } else if (rng < 0.33) {
+//     computerChoice = "S";
+//
+// }
+//
+//
+// var userWins = "You WIN!";
+// var computerWins = "The computer wins!";
+//
+// if(computerChoice !== userChoice){
+//     if(computerChoice === "R"){
+//         if (userChoice === "S") {
+//             alert(computerWins);
+//         } else {
+//             alert(userWins);
+//         }
+//     } else if (computerChoice === "P") {
+//         if( userChoice === "R") {
+//             alert(computerWins);
+//         } else {
+//             alert(userWins);
+//         }
+//     } else {
+//         if( userChoice === "P") {
+//             alert(computerWins);
+//         } else {
+//             alert(userWins);
+//         }
+//     }
+// } else {
+//     alert('Tie!');
+// }
 
 var userWins = "You win!";
 var computerWins = "The computer wins!";
-var roundCounter = 2;
+// var roundCounter = 2;
+
 
 var gameRound = function(){
     var userChoice = prompt("Choose Rock, Paper, or Scissors by typing 'R', 'P', or 'S'");
@@ -52,6 +53,9 @@ var gameRound = function(){
     } else if (rng > 0.33) {
         computerChoice = "S";
     }
+
+   
+
 
     if(computerChoice !== userChoice){
         if(computerChoice === "R"){
@@ -78,6 +82,7 @@ var gameRound = function(){
     }
 };
 
+var roundCounter = 0;
 while (roundCounter < 4) {
     gameRound();
     roundCounter++;
