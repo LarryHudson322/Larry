8df5eb5ac5904bc712b49d0e43102a3251270608
Larry Hudson <Lhudson322@gmail.com>
2018-02-18 09:19:32 -0500

    added links

--- exercise/while/while.js
+++ exercise/while/while.js
@@ -16,24 +16,24 @@ while (n <= 20) {
 }
 //
 // //EXERCISE 3: Create a while loop that console.logs a running total of the cumulative sum of numbers from 1 to n
-// var n = 100;
-// var i = 1;
-// var sum = 0;
-//
-// while (i < n) {
-//     sum += i;
-//     console.log(sum);
-//     i++;
+var n = 100;
+var i = 1;
+var sum = 0;
+
+while (i < n) {
+    sum += i;
+    console.log(sum);
+    i++;
 // }//EXERCISE 4: In your HTML document, create a separate div for each exercise. Then, in addition to console.log-ing each iteration, append all of the results to their respective divs using an unordered list. HINT (for exercise 1... try the others on your own)
-// var n = 1;
+var n = 1;
 // var outputTarget = document.querySelector("#exercise-1");
 // var outputHtml = "<ul>";
 //
-// while (n <= 10) {
-//     console.log(n);
-//     ouputHtml += "<li>" + n + "</li>";
-//     n++;
-// }
+while (n <= 10) {
+    console.log(n);
+    ouputHtml += "<li>" + n + "</li>";
+    n++;
+}
 //
 // outputHtml += "</ul>";
 //
@@ -60,23 +60,23 @@ while (n <= 20) {
 //
 //
 // //EXERCISE 6: Let's extend the idea of if and else in while loops with a pretty common exercise called FizzBuzz. For this exercise, log and output "Fizz" if a number is divisible by 3, "Buzz" if a number by 5, and "FizzBuzz" if a number is divisible by both 3 and 5. If a number is not divisible by 3 or 5, then just output the number. For this exercise, count up from 1 to 100
-// while (n <= 100) {
-//     if(n % 3 === 0 && n % 5 == 0){
-//         console.log("FizzBuzz");
-//         outputHtml += "<li>FizzBuzz</li>";
-//     } else if (n % 3 === 0) {
-//         console.log("Fizz");
-//         outputHtml += "<li>Fizz</li>";
-//     } else if (n % 5 === 0) {
-//         console.log("Buzz");
-//         outputHtml += "<li>Buzz</li>";
-//     } else {
-//         console.log(n);
-//         ouputHtml += "<li>" + n + "</li>";
-//     }
-//
-//     n++;
-// }
+while (n <= 100) {
+    if(n % 3 === 0 && n % 5 == 0){
+        console.log("FizzBuzz");
+        outputHtml += "<li>FizzBuzz</li>";
+    } else if (n % 3 === 0) {
+        console.log("Fizz");
+        outputHtml += "<li>Fizz</li>";
+    } else if (n % 5 === 0) {
+        console.log("Buzz");
+        outputHtml += "<li>Buzz</li>";
+    } else {
+        console.log(n);
+        ouputHtml += "<li>" + n + "</li>";
+    }
+
+    n++;
+}
 //
 // ouputHtml = "</ul>";
 //
